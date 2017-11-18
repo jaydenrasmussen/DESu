8594e2a1c1ed8f6f5161d7a9684e6bcaef094457
dylan <dylanswaters@gmail.com>
2017-11-18 15:13:15 -0700

    working fileIO and reassembly, huzzah!

--- src/cSourceFiles/reassembly.cpp
+++ src/cSourceFiles/reassembly.cpp
@@ -15,23 +15,60 @@ take out all of each file and place into new file
 delete old files
 */
 void reassemble(char directory[], char fileName[]){
+  //printf("executed reassemble\n");
   DIR *fileDir;
   struct dirent *epdf;
   fileDir = opendir(directory);
+  //printf("opened directory!\n");
   int currFile = 0;
   while ((epdf = readdir(fileDir)) != NULL){
+    //printf("at file #: %i\n", currFile);
+    string tempFileName = epdf->d_name;
+    if(tempFileName[0] != '.'){
+      currFile++;
+    }
+  }
+  closedir(fileDir);
+  //printf("number of parts: %i\n", currFile);
+
+  //loop for all files
+  FILE *ff;
+  ff = fopen(fileName, "w");
+  //printf("opened output file\n");
+  for(int i = 0; i < currFile; i++){
+    //printf("adding file #: %i\n", i);
+
+    FILE *nf;
+
+    string currFileStr = directory;
+    currFileStr += "/";
+    currFileStr += std::to_string(i);
+    //printf("current file: %s\n", currFileStr.c_str());
+    nf = fopen(currFileStr.c_str(), "r");
+    fseek(nf, 0, SEEK_END);
+    int size = ftell(nf);
+    for(int j = 0; j < size; j++){
+      fseek(nf, j, SEEK_SET);
+      int c = fgetc(nf);
+      //printf("character: %s\n", c);
+      fseek(ff, 0, SEEK_END);
+      fputc(c, ff);
+    }
+
+    //fclose(nf);
+  }
+  //fclose(ff);
+}
+
+
+/*
+    printf("current file: %i", currFile);
       //printf("%s\n",epdf->d_name);
       //string tempFileName = "./";
     string tempFileName = epdf->d_name;
       //printf("%s\n", tempFileName.c_str());
     string currFileNum = std::to_string(currFile);
-    string fileNum;
-    int strPos = 0;
-    while(currFileNum[strPos] != '%'){
-      fileNum += tempFileName[strPos];
-      strPos++;
-    }
-    if(currFileNum != fileNum)
+    if(currFileNum != tempFileName)
       continue;
     if(tempFileName[0] == '.')
       continue;
@@ -61,7 +98,7 @@ void reassemble(char directory[], char fileName[]){
   }
   currFile += 1;
 }
-
+*/
 
 /*
 usage:
